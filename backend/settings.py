48e0db0766ff074db63b701a6a7e1aeb698891e1
Patrick Ortiz <ortizpatrick750@gmail.com>
2026-08-04 22:50:46 -0500

    configurando conexion de despliegue

--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://softvar.adsoproject.dev',
+    'https://softvarapi.adsoproject.dev/'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -37,6 +39,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://softvar.adsoproject.dev',
+    'https://softvarapi.adsoproject.dev/'
 ]
 
 REST_FRAMEWORK = {
